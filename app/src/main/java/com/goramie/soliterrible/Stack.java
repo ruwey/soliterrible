8c4a87534c9e08a9503b4d52f2100b665de6ae92
AwareErmine <ksdjljgfd>
2022-03-21 12:23:52 -0400

    Add stack

--- app/src/main/java/com/goramie/soliterrible/Stack.java
+++ app/src/main/java/com/goramie/soliterrible/Stack.java
@@ -1,5 +1,19 @@
 package com.goramie.soliterrible;
 
-public class Stack {
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Stack {
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    public Stack() { }
+    public Stack(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void draw(int numShow) {
+        // Draw the whole stack, with number shown on top passed in
+    }
 }
