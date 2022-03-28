3ea96d4c36dd6945252fd37d180fa783f5f6149f
ruwey <gd@ruwey.com>
2022-03-27 23:32:17 -0400

    part way with dragging (dosn't work)

--- app/src/main/java/com/goramie/soliterrible/Card.java
+++ app/src/main/java/com/goramie/soliterrible/Card.java
@@ -3,7 +3,12 @@ package com.goramie.soliterrible;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Build;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import java.util.HashMap;
@@ -17,6 +22,7 @@ public class Card extends AppCompatImageView {
     private int type; // Even red, odd black
     private int num;
     private boolean showing = false;
+    Stack parent;
 
     static {
         TYPES = new HashMap<>();
@@ -26,9 +32,15 @@ public class Card extends AppCompatImageView {
         TYPES.put(2, "♦️");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public Card(Context c, int type, int num) {
         super(c);
         super.setImageResource(R.drawable.ic_card);
+        super.setOnDragListener((view, dragEvent) -> {
+            System.out.println("hello");
+
+            return true;
+        });
 
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setTextSize(30);
@@ -71,5 +83,8 @@ public class Card extends AppCompatImageView {
     public boolean toggleShow() {
         showing = !showing;
         return showing;
+    }
+    public void setParent(Stack s) {
+        parent = s;
     }
 }
